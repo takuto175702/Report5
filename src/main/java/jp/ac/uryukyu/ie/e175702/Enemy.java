7424b1877f17ac4fddb9321267b48bd44cc951c8
takuto175702 <Takuttyo0918>
2017-12-05 15:16:58 +0900

    Report5 Step3 Enemyをリファクタリング

--- src/main/java/jp/ac/uryukyu/ie/e175702/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175702/Enemy.java
@@ -26,4 +26,26 @@ public class Enemy extends LivingThing {
         }
     }
 
+    public void attack(LivingThing opponent){
+        if(isDead() == false) {
+            int damage = (int)(Math.random() * getAttack());
+
+            if(damage == 0){
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した!\n", getName(), opponent.getName(), damage);
+            }else {
+                int critical =(int)(Math.random()*10);
+
+                if(critical >= 0 && critical <= 4){
+                    damage =damage*2;
+                    System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                    opponent.wounded(damage);
+                }else{
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                    opponent.wounded(damage);
+                }
+            }
+        }else{
+
+        }
+    }
 }
